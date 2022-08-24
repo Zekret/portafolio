ea029a8266ac91b260d7da29d6290ee2000e0ec5
Zekret <benjaminconcha5@gmail.com>
2022-08-24 18:05:52 -0400

    update: two options style in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,14 @@ import Tabs from './Profile/Tabs';
 
 const Header = () => {
     return (
+        // <div
+        //     className='tw-p-4 tw-w-full tw-sticky tw-z-10 tw-top-0'>
+        //     <div className='tw-text-black tw-font-bold tw-flex tw-gap-4 tw-justify-center'>
+        //         <Tabs className='tw-rounded-full tw-bg-white tw-border tw-border-black tw-pr-4 tw-pl-4 tw-pt-2 tw-pb-2' label={'Sobre Mi'} value={939} />
+        //         <Tabs className='tw-rounded-full tw-bg-white tw-border tw-border-black tw-pr-4 tw-pl-4 tw-pt-2 tw-pb-2' label={'Habilidades'} value={1878} />
+        //         <Tabs className='tw-rounded-full tw-bg-white tw-border tw-border-black tw-pr-4 tw-pl-4 tw-pt-2 tw-pb-2' label={'Proyectos'} value={2817} />
+        //     </div>
+        // </div>
         <div
             className='tw-p-4 tw-bg-azul-main tw-w-full tw-sticky tw-z-10 tw-top-0'>
             <div className='tw-text-white tw-font-bold tw-flex tw-gap-4 tw-justify-center'>
