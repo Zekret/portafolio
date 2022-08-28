a98cda339d2ef79c0cdbb54f5402327bdbf33b57
Zekret <benjaminconcha5@gmail.com>
2022-08-27 20:06:10 -0400

    update: swiper in proyects.js

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import TitleMain from './TitleMain';
 import tenisup from '../assets/proyects/tenisup.png'
+import tenisup2 from '../assets/proyects/tenisup2.png'
+import tenisup3 from '../assets/proyects/tenisup3.png'
 import P from './FontType/P';
+
+//Swiper 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-flip";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { EffectFlip, Pagination, Navigation } from "swiper";
 
 const Proyects = ({ proyects, t }) => {
     return (
@@ -11,9 +25,24 @@ const Proyects = ({ proyects, t }) => {
             </div>
             <div className='tw-flex tw-flex-row tw-justify-center tw-items-center tw-w-full'>
                 <div className='tw-w-1/2'>
-                    <div className='tw-flex tw-place-content-center tw-p-10'>
-                        <img className='tw-shadow-3xl tw-rounded-lg tw-w-[400px] tw-h-[400px]' src={tenisup} alt='tenisup' />
-                    </div>
+                    <Swiper
+                        effect={"flip"}
+                        grabCursor={true}
+                        pagination={true}
+                        navigation={true}
+                        modules={[EffectFlip, Pagination, Navigation]}
+                        className="tw-flex tw-justify-center tw-items-center"
+                    >
+                        <SwiperSlide className='tw-flex tw-justify-center'>
+                            <img className='tw-shadow-3xl tw-rounded-lg tw-w-[400px] tw-h-[400px]' src={tenisup} alt='tenisup' />
+                        </SwiperSlide>
+                        <SwiperSlide className='tw-flex tw-justify-center'>
+                            <img className='tw-shadow-3xl tw-rounded-lg tw-w-[400px] tw-h-[400px]' src={tenisup2} alt='tenisup' />
+                        </SwiperSlide>
+                        <SwiperSlide className='tw-flex tw-justify-center'>
+                            <img className='tw-shadow-3xl tw-rounded-lg tw-w-[400px] tw-h-[400px]' src={tenisup3} alt='tenisup' />
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
                 <div className='tw-w-1/2'>
                     <div className='tw-px-20'>
