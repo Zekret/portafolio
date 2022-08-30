0505ed597309a1e9311cf802d06df1cd157457ee
Zekret <benjaminconcha5@gmail.com>
2022-08-30 13:19:40 -0400

    fix: header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,6 @@ import React from 'react';
 import '../styles/header.css'
 const Header = ({ HandleAbout, HandleSkills, HandleProyects, t }) => {
     return (
-        // <div
-        //     className='tw-p-4 tw-w-full tw-sticky tw-z-10 tw-top-0'>
-        //     <div className='tw-text-black tw-font-bold tw-flex tw-gap-4 tw-justify-center'>
-        //         <div className='container-button'>
-        //             <div onClick={HandleAbout} className='tw-cursor-pointer tw-flex tw-flex-row tw-bg-transparent hover:tw-bg-blue-500 tw-text-blue-700 tw-font-semibold hover:tw-text-white tw-py-2 tw-px-6 tw-border tw-border-blue-500 hover:tw-border-transparent tw-rounded'>{t("header.about")}</div>
-        //         </div>
-        //         <div className='container-button'>
-        //             <div onClick={HandleSkills} className='tw-cursor-pointer tw-flex tw-flex-row tw-bg-transparent hover:tw-bg-blue-500 tw-text-blue-700 tw-font-semibold hover:tw-text-white tw-py-2 tw-px-6 tw-border tw-border-blue-500 hover:tw-border-transparent tw-rounded'>{t("header.skills")}</div>
-        //         </div>
-        //         <div className='container-button'>
-        //             <div onClick={HandleProyects} className='tw-cursor-pointer tw-flex tw-flex-row tw-bg-transparent hover:tw-bg-blue-500 tw-text-blue-700 tw-font-semibold hover:tw-text-white tw-py-2 tw-px-6 tw-border tw-border-blue-500 hover:tw-border-transparent tw-rounded'>{t("header.proyects")}</div>
-        //         </div>
-        //     </div>
-        // </div>
         <div
             className='tw-p-4 tw-bg-azul-main tw-w-full tw-sticky tw-z-10 tw-top-0'>
             <div className='tw-text-white tw-font-bold tw-flex tw-gap-4 tw-justify-center'>
